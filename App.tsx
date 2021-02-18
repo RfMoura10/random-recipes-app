400fe3c4636b5b03173a70438fcabbe53c4eeec1
Rafael Moura <alves_rmoura@hotmail.com>
2021-02-17 23:58:07 -0300

    first page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { Title, Subtitle, Paragraph} from "./components/Typography";
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Main = styled.SafeAreaView`
+  flex-grow: 1;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+const Scroll = styled.ScrollView`
+  flex-grow: 1;
+  width: 100%;
+`;
+
+const Content = styled.View`
+  padding: 0 20px;
+`;
+
+const Space = styled.View`
+  margin-bottom: ${({size} : {size?:1|2}) => ((size ?? 2) * 10) + 'px' };
+`;
+
+const Line = styled.View`
+  height: 3px;
+  width: 100%;
+  background-color: #DF8320;
+  opacity: .3;
+`;
+
+const IngredientsList = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 5px;
+`
+const MainImage = styled.ImageBackground`
+  height: 35%;
+  width: 100%;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Main>
+      <Scroll>
+        <MainImage source={{uri: 'https://www.themealdb.com/images/media/meals/1550440197.jpg'}}>
+          <LinearGradient colors={['transparent', 'transparent', '#fff']} style={{width:'100%', height:'100%'}}/>
+        </MainImage>
+        <Space/>
+        <Content>
+          <Title>Chicken Couscou</Title>
+          <Line/>
+          <Space/>
+          <Subtitle>Ingredients</Subtitle>
+          <Space size={1}/>
+          {
+            [...Array(10).fill(0)].map((_,i) => 
+              <IngredientsList key={i}>
+                <Paragraph>Olive Oil </Paragraph>
+                <Paragraph secondary>1 chopped </Paragraph>
+              </IngredientsList>
+            )
+          }
+          <Space/>
+
+          <Subtitle>Prepare</Subtitle>
+          <Space size={1}/>
+          <Paragraph>
+          First make the Hollandaise sauce. Put the lemon juice and vinegar in a small bowl, add the egg yolks and whisk with a balloon whisk until light and frothy. Place the bowl over a pan of simmering water and whisk until mixture thickens. Gradually add the butter, whisking constantly until thick – if it looks like it might be splitting, then whisk off the heat for a few mins. Season and keep warm.\r\n\r\nTo poach the eggs, bring a large pan of water to the boil and add the vinegar. Lower the heat so that the water is simmering gently. Stir the water so you have a slight whirlpool, then slide in the eggs one by one. Cook each for about 4 mins, then remove with a slotted spoon.\r\n\r\nLightly toast and butter the muffins, then put a couple of slices of salmon on each half. Top each with an egg, spoon over some Hollandaise and garnish with chopped chives.
+          </Paragraph>
+        </Content>
+      </Scroll>
+
       <StatusBar style="auto" />
-    </View>
+    </Main>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Typography.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import { useFonts } from 'expo-font';
+import styled from 'styled-components';
+
+type Props = {
+  children: React.ReactNode
+}
+
+type Props2 = {
+  children: React.ReactNode,
+  secondary?: boolean
+}
+
+export function Title({children} : Props) {
+    const [loaded] = useFonts({
+      BioRhyme: require('../assets/fonts/BioRhyme-Bold.ttf'),
+    });
+    
+    if (!loaded) {
+      return null;
+    }
+  
+    return <>
+        <Text style={{ fontFamily: 'BioRhyme', fontSize: 24, textAlign:"left", color:'#DF8320' }}>
+            { children }
+        </Text>
+    </>;
+  }
+
+export function Subtitle({children} : Props) {
+    const [loaded] = useFonts({
+      BioRhyme: require('../assets/fonts/BioRhyme-Bold.ttf'),
+    });
+    
+    if (!loaded) {
+      return null;
+    }
+  
+    return <>
+        <Text style={{ fontFamily: 'BioRhyme', fontSize: 16, textAlign:"left", color:"#000", opacity:.8 }}>
+            { children }
+        </Text>
+    </>;
+  }
+
+export function Paragraph({children, secondary} : Props2) {
+    const [loaded] = useFonts({
+      Cabin: require('../assets/fonts/Cabin-Bold.ttf'),
+    });
+    
+    if (!loaded) {
+      return null;
+    }
+  
+    return <>
+        <Text style={{ fontFamily: 'Cabin', fontSize: 14, textAlign:"left", lineHeight:21, color:"#000", opacity: secondary ? .5 : .8 }}>
+            { children }
+        </Text>
+    </>;
+  }
